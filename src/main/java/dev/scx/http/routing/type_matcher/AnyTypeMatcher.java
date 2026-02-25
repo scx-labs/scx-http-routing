695a2e3a63bd05f7c5e50592d6bf630140d5ea3a
scx567888 <scx567888@outlook.com>
2026-02-25 18:09:26 +0800

    dev

--- src/main/java/dev/scx/http/routing/type_matcher/AnyTypeMatcher.java
+++ src/main/java/dev/scx/http/routing/type_matcher/AnyTypeMatcher.java
@@ -2,6 +2,10 @@ package dev.scx.http.routing.type_matcher;
 
 import dev.scx.http.ScxHttpServerRequest;
 
+/// AnyTypeMatcher
+///
+/// @author scx567888
+/// @version 0.0.1
 final class AnyTypeMatcher implements TypeMatcher {
 
     public static final AnyTypeMatcher ANY_TYPE_MATCHER = new AnyTypeMatcher();

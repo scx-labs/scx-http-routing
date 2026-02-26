2832c640e50c1638dbd1fc15afcfa09f7eb0cddd
scx567888 <scx567888@outlook.com>
2026-02-26 14:36:19 +0800

    dev

--- src/main/java/dev/scx/http/routing/RouteBuilder.java
+++ src/main/java/dev/scx/http/routing/RouteBuilder.java
@@ -2,12 +2,12 @@ package dev.scx.http.routing;
 
 import dev.scx.function.Function1Void;
 import dev.scx.http.ScxHttpServerRequest;
-import dev.scx.http.method.HttpMethod;
+import dev.scx.http.method.ScxHttpMethod;
 import dev.scx.http.routing.method_matcher.MethodMatcher;
 import dev.scx.http.routing.path_matcher.PathMatcher;
 import dev.scx.http.routing.type_matcher.TypeMatcher;
 
-/// RouteWritable
+/// RouteBuilder
 ///
 /// @author scx567888
 /// @version 0.0.1
@@ -27,11 +27,13 @@ public final class RouteBuilder {
         this.handler = null;
     }
 
+    // ***************** order 相关 *********************
     public RouteBuilder order(int order) {
         this.order = order;
         return this;
     }
 
+    // ***************** typeMatcher 相关 *********************
     public RouteBuilder typeMatcher(TypeMatcher typeMatcher) {
         this.typeMatcher = typeMatcher;
         return this;
@@ -45,6 +47,7 @@ public final class RouteBuilder {
         return typeMatcher(TypeMatcher.not(requestType));
     }
 
+    // ***************** pathMatcher 相关 *********************
     public RouteBuilder pathMatcher(PathMatcher pathMatcher) {
         this.pathMatcher = pathMatcher;
         return this;
@@ -58,20 +61,23 @@ public final class RouteBuilder {
         return pathMatcher(PathMatcher.ofRegex(regex));
     }
 
+    // ***************** methodMatcher 相关 *********************
     public RouteBuilder methodMatcher(MethodMatcher methodMatcher) {
         this.methodMatcher = methodMatcher;
         return this;
     }
 
-    public RouteBuilder method(HttpMethod... httpMethods) {
+    public RouteBuilder method(ScxHttpMethod... httpMethods) {
         return methodMatcher(MethodMatcher.of(httpMethods));
     }
 
+    // ***************** handler 相关 *********************
     public RouteBuilder handler(Function1Void<RoutingContext, ?> handler) {
         this.handler = handler;
         return this;
     }
 
+    // ***************** build *********************
     public Route build() {
         return new RouteImpl(
             order,

577da1032d32439723fe4684d4bb3daa1244ab99
scx567888 <scx567888@outlook.com>
2026-02-26 14:30:48 +0800

    dev

--- src/main/java/dev/scx/http/routing/Router.java
+++ src/main/java/dev/scx/http/routing/Router.java
@@ -16,6 +16,10 @@ public interface Router extends Function1Void<ScxHttpServerRequest, Throwable> {
     }
 
     /// 添加一个路由
+    /// 路由按 [Route#order()] 从小到大匹配:
+    ///
+    /// - order 越小, 匹配优先级越高.
+    /// - order 相同的路由, 按注册顺序匹配.
     Router add(Route route);
 
     /// 移除一个路由

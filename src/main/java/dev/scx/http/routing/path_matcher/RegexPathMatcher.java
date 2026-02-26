5b5e6978078c2ad75ab7d6eb3bb5e54403d6d517
scx567888 <scx567888@outlook.com>
2026-02-26 17:08:17 +0800

    dev

--- src/main/java/dev/scx/http/routing/path_matcher/RegexPathMatcher.java
+++ src/main/java/dev/scx/http/routing/path_matcher/RegexPathMatcher.java
@@ -3,10 +3,13 @@ package dev.scx.http.routing.path_matcher;
 import java.util.Map;
 import java.util.regex.Pattern;
 
+/// RegexPathMatcher
+///
+/// @author scx567888
+/// @version 0.0.1
 final class RegexPathMatcher implements PathMatcher {
 
     private final Pattern pattern;
-    // 这里应该是一个不可变 map.
     private final Map<String, Integer> namedGroups;
 
     public RegexPathMatcher(String regex) {
@@ -31,6 +34,7 @@ final class RegexPathMatcher implements PathMatcher {
             groups[i] = matcher.group(i + 1); // 注意 group 索引是 1 起始.
         }
 
+        // pattern.namedGroups() 返回的是不可变的 Map 这里可以安全传递.
         return new RegexPathMatch(groups, namedGroups);
     }
 

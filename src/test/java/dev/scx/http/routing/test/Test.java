50af671ad24e6099a691e68ce7893081767bcca3
scx567888 <scx567888@outlook.com>
2026-02-26 15:06:49 +0800

    dev

--- src/test/java/dev/scx/http/routing/test/Test.java
+++ src/test/java/dev/scx/http/routing/test/Test.java
@@ -1,12 +1,15 @@
 package dev.scx.http.routing.test;
 
+import dev.scx.http.ScxHttpServer;
 import dev.scx.http.exception.UnauthorizedException;
+import dev.scx.http.routing.RouteBuilder;
 import dev.scx.http.routing.Router;
 
 import java.io.IOException;
 
 import static dev.scx.http.method.HttpMethod.GET;
 import static dev.scx.http.method.HttpMethod.POST;
+import static dev.scx.http.routing.RouteBuilder.*;
 
 // todo 待改造
 public class Test {
@@ -23,41 +26,41 @@ public class Test {
 
 //        router.route(-100000).handler(new CorsHandler().addOrigin("http://localhost:18899"));
 
-        router.route().path("/abc").handler(c -> {
+        router.add(route().path("/abc").handler(c -> {
             var bytes = c.request().asBytes();
             System.out.println(bytes.length);
 //            var bbb=c.request().body().asMultiPart();
 //            for (var aByte : bbb) {
 //                System.out.println(aByte.name()+" "+wrap(()-> aByte.inputStream().readAllBytes().length));
 //            }
-            c.response().send("12312313");
+            c.request().response().send("12312313");
 //            c.next();
-        });
+        }));
 
-        router.route().path("/*").handler(c -> {
+        router.add(route().path("/*").handler(c -> {
             System.out.println(c.request().path());
             c.next();
-        });
+        }));
 
-        router.route().path("/hello").method(GET).handler(c -> {
-            c.response().send("hello");
-        });
+        router.add(route().path("/hello").method(GET).handler(c -> {
+            c.request().response().send("hello");
+        }));
 
-        router.route().path("/path-params/:id").method(GET).handler(c -> {
-            c.response().send("id : " + c.pathMatch().get("id"));
-        });
+        router.add(route().path("/path-params/:id").method(GET).handler(c -> {
+            c.request().response().send("id : " + c.pathMatch().get("id"));
+        }));
 
-        router.route().path("/401").method(GET).handler(c -> {
+        router.add(route().path("/401").method(GET).handler(c -> {
             throw new UnauthorizedException();
-        });
+        }));
 
-        router.route().path("/405").method(POST).handler(c -> {
+        router.add(route().path("/405").method(POST).handler(c -> {
             System.out.println("405");
-        });
+        }));
 
-        router.route().path("/last").method(GET).handler(c -> {
+        router.add(route().path("/last").method(GET).handler(c -> {
             var r = 1 / 0;
-        });
+        }));
 
 //        server.onRequest(router);
 

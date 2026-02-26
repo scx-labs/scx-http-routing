50579810946fd583741b89127ed02270874eee14
scx567888 <scx567888@outlook.com>
2026-02-26 10:55:27 +0800

    dev

--- src/main/java/dev/scx/http/routing/RouteWritable.java
+++ src/main/java/dev/scx/http/routing/RouteWritable.java
@@ -5,7 +5,8 @@ import dev.scx.http.ScxHttpServerRequest;
 import dev.scx.http.method.HttpMethod;
 import dev.scx.http.routing.type_matcher.TypeMatcher;
 
-import static dev.scx.http.routing.type_matcher.TypeMatcher.*;
+import static dev.scx.http.routing.type_matcher.TypeMatcher.is;
+import static dev.scx.http.routing.type_matcher.TypeMatcher.not;
 
 /// RouteWritable
 ///
@@ -25,11 +26,11 @@ public interface RouteWritable extends Route {
 
     RouteWritable handler(Function1Void<RoutingContext, ?> handler);
 
-    default RouteWritable typeIs(Class<? extends ScxHttpServerRequest> requestType){
+    default RouteWritable typeIs(Class<? extends ScxHttpServerRequest> requestType) {
         return type(is(requestType));
     }
 
-    default RouteWritable typeNot(Class<? extends ScxHttpServerRequest> requestType){
+    default RouteWritable typeNot(Class<? extends ScxHttpServerRequest> requestType) {
         return type(not(requestType));
     }
 

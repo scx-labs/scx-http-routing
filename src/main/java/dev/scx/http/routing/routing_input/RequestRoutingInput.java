1bb21ff38e63fac86f85755fc8bb76a3305324ad
scx567888 <scx567888@outlook.com>
2026-02-27 17:43:11 +0800

    dev

--- src/main/java/dev/scx/http/routing/routing_input/RequestRoutingInput.java
+++ src/main/java/dev/scx/http/routing/routing_input/RequestRoutingInput.java
@@ -1,8 +1,6 @@
 package dev.scx.http.routing.routing_input;
 
 import dev.scx.http.ScxHttpServerRequest;
-import dev.scx.http.headers.ScxHttpHeaders;
-import dev.scx.http.method.ScxHttpMethod;
 import dev.scx.http.uri.ScxURI;
 
 final class RequestRoutingInput implements RoutingInput {
@@ -14,18 +12,8 @@ final class RequestRoutingInput implements RoutingInput {
     }
 
     @Override
-    public ScxHttpMethod method() {
-        return request.method();
-    }
-
-    @Override
     public ScxURI uri() {
         return request.uri();
-    }
-
-    @Override
-    public ScxHttpHeaders headers() {
-        return request.headers();
     }
 
 }
--- src/main/java/dev/scx/http/routing/routing_input/RoutingInput.java
+++ src/main/java/dev/scx/http/routing/routing_input/RoutingInput.java
@@ -1,22 +1,34 @@
 package dev.scx.http.routing.routing_input;
 
 import dev.scx.http.ScxHttpServerRequest;
-import dev.scx.http.headers.ScxHttpHeaders;
-import dev.scx.http.method.ScxHttpMethod;
 import dev.scx.http.parameters.Parameters;
 import dev.scx.http.uri.ScxURI;
 
+/// RoutingInput 表示路由系统在进行匹配与分流时所使用的 "输入视图".
+///
+/// 该抽象刻意只建模 URI (主要是 path), 用于支持路由组合场景,
+/// 例如子路由, 路径前缀处理以及路径重写等.
+///
+/// 虽然从理论上讲,HTTP headers 和 method 也可以作为路由视图的一部分,
+/// 但在本设计中被有意识地排除:
+///
+/// - headers 被视为原始请求 (request) 的事实属性,
+///   通过 `RequestMatcher` 在事实层面进行匹配,
+///   而不是作为可重写的路由视图.
+///
+/// - HTTP method 的重写在实际 Web 路由场景中意义有限,
+///   因此不作为 RoutingInput 的建模内容.
+///
+/// 该取舍旨在保持 `RoutingInput` 的职责单一和语义清晰,
+/// 避免其演化为另一个 request 抽象,
+/// 同时仍然覆盖路由系统中唯一具有普遍实际价值的视图变换能力: 路径(URI)重写.
 public interface RoutingInput {
 
     static RoutingInput of(ScxHttpServerRequest request) {
         return new RequestRoutingInput(request);
     }
 
-    ScxHttpMethod method();
-
     ScxURI uri();
-
-    ScxHttpHeaders headers();
 
     //******************** 简化 URI 操作 *******************
 

8fbaeb927568d238f312e15fd3cb879fb5ed9d79
scx567888 <scx567888@outlook.com>
2026-02-27 16:23:13 +0800

    dev

--- src/main/java/dev/scx/http/routing/Route.java
+++ src/main/java/dev/scx/http/routing/Route.java
@@ -3,7 +3,7 @@ package dev.scx.http.routing;
 import dev.scx.function.Function1Void;
 import dev.scx.http.routing.method_matcher.MethodMatcher;
 import dev.scx.http.routing.path_matcher.PathMatcher;
-import dev.scx.http.routing.type_matcher.TypeMatcher;
+import dev.scx.http.routing.request_matcher.RequestMatcher;
 
 /// Route 路由只保存状态不做行为处理
 ///
@@ -14,7 +14,7 @@ public interface Route {
     /// 路由优先级: 数值越小越先匹配, 相同 order 按注册顺序匹配.
     int order();
 
-    TypeMatcher typeMatcher();
+    RequestMatcher requestMatcher();
 
     PathMatcher pathMatcher();
 

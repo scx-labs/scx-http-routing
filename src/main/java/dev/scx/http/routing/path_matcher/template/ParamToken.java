b6d318f0c9274146480e19c84fbb985439f38d71
scx567888 <scx567888@outlook.com>
2026-02-28 17:08:04 +0800

    dev

--- src/main/java/dev/scx/http/routing/path_matcher/template/ParamToken.java
+++ src/main/java/dev/scx/http/routing/path_matcher/template/ParamToken.java
@@ -2,4 +2,13 @@ package dev.scx.http.routing.path_matcher.template;
 
 record ParamToken(String name) implements Token {
 
+    ParamToken {
+        if (name.isEmpty()) {
+            throw new IllegalArgumentException("param name must not be empty");
+        }
+        if ("*".equals(name)) {
+            throw new IllegalArgumentException("param name can not be '*'");
+        }
+    }
+
 }

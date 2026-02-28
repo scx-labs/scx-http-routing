276266f0a557e030fff33c12b17d3f72e2f132b8
scx567888 <scx567888@outlook.com>
2026-02-28 16:12:55 +0800

    dev

---
+++ src/main/java/dev/scx/http/routing/path_matcher/IndexedPathMatch.java
@@ -0,0 +1,37 @@
+package dev.scx.http.routing.path_matcher;
+
+import java.util.Map;
+
+/// IndexedPathMatch
+///
+/// @author scx567888
+/// @version 0.0.1
+final class IndexedPathMatch implements PathMatch {
+
+    private final String[] values;
+    private final Map<String, Integer> nameToIndex;
+
+    public IndexedPathMatch(String[] values, Map<String, Integer> nameToIndex) {
+        this.values = values;
+        this.nameToIndex = nameToIndex;
+    }
+
+    @Override
+    public String capture(int index) {
+        // 不在范围内.
+        if (index < 0 || index >= values.length) {
+            return null;
+        }
+        return values[index];
+    }
+
+    @Override
+    public String capture(String name) {
+        var i = nameToIndex.get(name);
+        if (i == null) {
+            return null;
+        }
+        return capture(i);
+    }
+
+}
